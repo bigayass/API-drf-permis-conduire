86cd1981b2b9d2635eb4c956397289d31ecee110
Yasser <yasserazelmad@gmail.com>
2022-03-17 23:30:20 +0100

    permis-conduire

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['permis-conduire.hrokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['permis-conduire.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
